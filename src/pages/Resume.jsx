bf0f593c2600ab2036800505098bb6abd503bd5f
daiterranova <daiana.terranova@gmail.com>
2023-09-25 12:52:59 -0300

    added general structure

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,35 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 
 const Resume = () => {
-  return (<div>Resume</div>)
+
+  const containerStyles ={
+    boxShadow: 3,
+    alignItems:'center',
+    flexDirection:'column',
+    marginBottom:'2rem', 
+    minHeight:'100vh',
+    padding:'2rem'
+  }
+  return (
+  <>
+    <Grid container sx={containerStyles} className="container">
+      <Grid container item>
+        <Grid item> <Typography variant="h5">About me</Typography></Grid>
+        <Grid item>text</Grid>
+      </Grid>
+      <Grid container item>
+        <Grid item> <Typography></Typography></Grid>
+        <Grid item> cards</Grid>
+      </Grid>
+      <Grid container item>
+        <Grid item> <Typography></Typography></Grid>
+        <Grid item>skills</Grid>
+      </Grid>
+    </Grid>
+
+  </>
+)
 };
 
 export default Resume;
