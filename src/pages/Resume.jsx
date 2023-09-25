1c4f41c971adbd919362d377489bbc471d23cdb7
daiterranova <daiana.terranova@gmail.com>
2023-09-25 14:45:03 -0300

    add about me section on resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,6 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { about } from "../utils/resumeData";
 
 const Resume = () => {
 
@@ -8,20 +9,35 @@ const Resume = () => {
     alignItems:'center',
     flexDirection:'column',
     marginBottom:'2rem', 
+    gap:'3rem',
     minHeight:'100vh',
     padding:'2rem'
   }
+
+  const titleStyles ={
+    fontWeight:500,
+  }
   return (
   <>
+  {/* about me */}
     <Grid container sx={containerStyles} className="container">
       <Grid container item>
-        <Grid item> <Typography variant="h5">About me</Typography></Grid>
-        <Grid item>text</Grid>
+        <Grid item xs={12}> 
+          <Typography gutterBottom  variant="h5">About me</Typography>
+        </Grid>
+        <Grid item xs={12}  className="description-color" >{about}</Grid>
       </Grid>
-      <Grid container item>
+    {/* education and experiences */}
+    <Grid container item>
+        <Grid item> work</Grid>
+        <Grid item> education</Grid>
+      </Grid>
+    {/* services */}
+    <Grid container item>
         <Grid item> <Typography></Typography></Grid>
         <Grid item> cards</Grid>
       </Grid>
+    {/* skills */}
       <Grid container item>
         <Grid item> <Typography></Typography></Grid>
         <Grid item>skills</Grid>
