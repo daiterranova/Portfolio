15ed3e72217e0db5d486aba8e3b85c4a7f26ae89
daiterranova <daiana.terranova@gmail.com>
2023-09-26 12:35:06 -0300

    implement timeline to render work experience and education info

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,12 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
 import { about } from "../utils/resumeData";
+import CustomTimeline from "../components/Timeline";
+import { experiences, education } from "../utils/resumeData";
+import '../components/profile.css'
+import WorkIcon from '@mui/icons-material/Work';
+import SchoolIcon from '@mui/icons-material/School';
+import {CustomTimelineItemData} from '../components/Profile'
 
 const Resume = () => {
 
@@ -23,15 +29,33 @@ const Resume = () => {
     <Grid container sx={containerStyles} className="container">
       <Grid container item>
         <Grid item xs={12}> 
-          <Typography gutterBottom  variant="h5">About me</Typography>
+          <Typography gutterBottom variant="h5">About me</Typography>
         </Grid>
-        <Grid item xs={12}  className="description-color" >{about}</Grid>
+        <Grid item xs={12}  className="description-color" fontSize={16} >{about}</Grid>
       </Grid>
     {/* education and experiences */}
-    <Grid container item>
-        <Grid item> work</Grid>
-        <Grid item> education</Grid>
-      </Grid>
+    <Grid container item spacing={4}>
+        <Grid item xs={12}> 
+          <Typography gutterBottom  variant="h5">Resume</Typography>
+        </Grid>
+        <Grid container item>
+          <Grid item xs={12} md={6}> 
+            <CustomTimeline  title='Work Experience' icon={<WorkIcon/>}>
+                {experiences.map((experience) => (
+                  <CustomTimelineItemData  title={experience.title} text={experience.description} date={experience.date} experience={true} profile={false} />
+                  ))}
+            </CustomTimeline>
+          </Grid>
+          <Grid item xs={12} md={6}> 
+          <CustomTimeline  title='Education' icon={<SchoolIcon/>}>
+                {education.map((education) => (
+                  <CustomTimelineItemData title={education.title} text={education.description} date={education.date} education={true} profile={false} />
+                  ))}
+            </CustomTimeline>
+          </Grid>
+        </Grid>
+        
+     </Grid>
     {/* services */}
     <Grid container item>
         <Grid item> <Typography></Typography></Grid>
