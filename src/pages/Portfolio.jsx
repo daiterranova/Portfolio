7a76c75aede7533b890c606c32988e4934ec3dc4
daiterranova <daiana.terranova@gmail.com>
2023-09-27 17:08:16 -0300

    implement project cards

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import CssBaseline from "@mui/material/CssBaseline";
 import Box from '@mui/material/Box';
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Tabs, Tab, Card, CardActionArea, CardMedia, CardContent, Grow } from '@mui/material';
 import { projects } from "../utils/resumeData";
-
 
 const Portfolio = () => {
   const [tabValue, setTabValue] = useState('All');
@@ -28,29 +26,55 @@ const Portfolio = () => {
           <Typography gutterBottom variant="h5">Portfolio</Typography>
         </Grid>
       <Grid item sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'var(--surface)' }} >
-      <Tabs
-        value={tabValue}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs"
-        sx={{
-          '& .MuiTabs-indicator': {
-            backgroundColor: 'var(--secondary)', // Change indicator color here
-          },
-          '& .MuiTab-root': {
-            color: 'var(--primary)', 
-          },
-        }}
-      >
-        <Tab label="All" value='All' className={tabValue ? 'custom-tabs active' : 'custom-tabs'} />
-       {
-        [... new Set(projects.map(project => project.tag))].map(tag => (
-          <Tab label={tag} value={tag} className={tabValue ? 'custom-tabs active' : 'custom-tabs'}/>
-        ))
-       }
-      </Tabs>
-    </Grid>
+        <Tabs
+          value={tabValue}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs"
+          sx={{
+            '& .MuiTabs-indicator': {
+              backgroundColor: 'var(--secondary)', // Change indicator color here
+            },
+            '& .MuiTab-root': {
+              color: 'var(--primary)', 
+            },
+          }}
+        >
+          <Tab label="All" value='All' className={tabValue ? 'custom-tabs active' : 'custom-tabs'} />
+        {
+          [... new Set(projects.map(project => project.tag))].map(tag => (
+            <Tab label={tag} value={tag} className={tabValue ? 'custom-tabs active' : 'custom-tabs'}/>
+          ))
+        }
+        </Tabs>
+      </Grid>
+      <Grid container item spacing={3} alignItems='center' justifyContent='center' >
+        {projects.map((project) => (
+          <Grid container item xs={6} key={project.image}>
+            <Grow in timeout={1000}>
+              <Card sx={{ width:'100%' }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={project.image}
+                        alt={project.title}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {project.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        {project.description}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+              </Card>
+            </Grow>
+          </Grid>
+        ))}
+      </Grid>
     </Grid>
    
 )
